115172a57f8bdce4dd44f48c189c8196661ffeef
Maciej Lasyk <maciej@lasyk.info>
2017-10-01 21:35:22 +0200

    AppEngine Config extracted to config file

--- appengine-image-updater/main.py
+++ appengine-image-updater/main.py
@@ -1,8 +1,12 @@
-import webapp2
+import os
+import sys
 import logging
-from google.appengine.ext import vendor
+from ConfigParser import SafeConfigParser
+import webapp2
 
+from google.appengine.ext import vendor
 vendor.add('lib')
+
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 
@@ -14,13 +18,37 @@ class Main(webapp2.RequestHandler):
 
 
 class Updater(webapp2.RequestHandler):
-    def get(self):
-        credentials = GoogleCredentials.get_application_default()
-        service = discovery.build('compute', 'beta', credentials=credentials)
+    def __init__(self, request, response):
+        super(Updater, self).__init__(request, response)
+        self._parse_config()
 
-        project = 'maciej-lasyk-info'
-        region = 'us-central1'
-        instance_group_manager = 'ml-instance-group-manager'
+    def _parse_config(self):
+        config_path = os.path.dirname(os.path.realpath(__file__))+'/config.ini'
+
+        parser = SafeConfigParser()
+
+        if not os.path.exists(config_path):
+            logging.info(config_path)
+            logging.info('WTF?')
+        try:
+            parser.read(config_path)
+        except Exception as e:
+            logging.error(e)
+
+        self.project = parser.get('project', 'project_id')
+        self.region = parser.get('project', 'region')
+        self.instance_group_manager = parser.get('project', 'instance_group_manager')
+
+    def get(self):
+        try:
+            credentials = GoogleCredentials.get_application_default()
+        except Exception as e:
+            logging.error(e)
+
+        try:
+            service = discovery.build('compute', 'beta', credentials=credentials)
+        except Exception as e:
+            logging.error(e)
 
         instance_group_manager_body = {
             "updatePolicy": {
@@ -36,14 +64,15 @@ class Updater(webapp2.RequestHandler):
             },
             "versions": [
                 {
-                    "instanceTemplate": "projects/maciej-lasyk-info/global/instanceTemplates/ml-instance-template"
+                    "instanceTemplate": "projects/maciej-lasyk-info/global/instanceTemplates/ml-instance-template",
+                    "name": "v1"
                 }
             ]
         }
 
         request = service.regionInstanceGroupManagers().patch(
-            project=project, region=region,
-            instanceGroupManager=instance_group_manager,
+            project=self.project, region=self.region,
+            instanceGroupManager=self.instance_group_manager,
             body=instance_group_manager_body
         )
         response = request.execute()
