5ae8dfe2f1cc9c084b31a93f6fbc7896b72ea4e0
Maciej Lasyk <maciej@lasyk.info>
2017-12-14 22:03:20 +0100

    /sport cd

--- terraform/scripts/update_maciej.lasyk.info-sport.sh
+++ terraform/scripts/update_maciej.lasyk.info-sport.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 GIT=/usr/bin/git
 
-export PROJDIR=/srv/maciej.lasyk.info/pelican-sport
+export PROJDIR=/srv/maciej.lasyk.info
 cd ${PROJDIR}
 $GIT fetch --all
 $GIT reset --hard origin/master
 docker rm pelican-sport &> /dev/null
-docker run --name pelican-sport -v ${PROJDIR}/pelican/content:/srv/content:Z \
-    -v ${PROJDIR}/pelican/output:/srv/output:Z \
-    -v ${PROJDIR}/pelican/pelicanconf.py:/srv/pelicanconf.py:Z \
-    -v ${PROJDIR}/pelican/publishconf.py:/srv/publishconf.py:Z \
-    -v ${PROJDIR}/pelican/themes:/srv/themes:Z \
-    -v ${PROJDIR}/pelican/plugins:/srv/plugins:Z gcr.io/maciej-lasyk-info/pelican-f25.1
+docker run --name pelican-sport -v ${PROJDIR}/pelican-sport/content:/srv/content:Z \
+    -v ${PROJDIR}/pelican-sport/output:/srv/output:Z \
+    -v ${PROJDIR}/pelican-sport/pelicanconf.py:/srv/pelicanconf.py:Z \
+    -v ${PROJDIR}/pelican-sport/publishconf.py:/srv/publishconf.py:Z \
+    -v ${PROJDIR}/pelican-sport/themes:/srv/themes:Z \
+    -v ${PROJDIR}/pelican-sport/plugins:/srv/plugins:Z gcr.io/maciej-lasyk-info/pelican-f25.1
