8dd56050aaa4a8847ff12fe45105c6ce6878fbd1
Maciej Lasyk <maciej@lasyk.info>
2017-12-14 21:53:38 +0100

    /sport cd

--- terraform/scripts/update_maciej.lasyk.info-sport.sh
+++ terraform/scripts/update_maciej.lasyk.info-sport.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 GIT=/usr/bin/git
 
-export PROJDIR=/srv/maciej.lasyk.info-sport
+export PROJDIR=/srv/maciej.lasyk.info/sport
 cd ${PROJDIR}
 $GIT fetch --all
 $GIT reset --hard origin/master
@@ -11,4 +11,4 @@ docker run --name pelican -v ${PROJDIR}/pelican/content:/srv/content:Z \
     -v ${PROJDIR}/pelican/pelicanconf.py:/srv/pelicanconf.py:Z \
     -v ${PROJDIR}/pelican/publishconf.py:/srv/publishconf.py:Z \
     -v ${PROJDIR}/pelican/themes:/srv/themes:Z \
-    -v ${PROJDIR}/pelican/plugins:/srv/plugins:Z gcr.io/maciej-lasyk-info-sport/pelican-f25.1
+    -v ${PROJDIR}/pelican/plugins:/srv/plugins:Z gcr.io/maciej-lasyk-info/pelican-f25.1
