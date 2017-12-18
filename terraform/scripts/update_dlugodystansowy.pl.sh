ba58760c5c5f03e90df59d7637574cee794c2e8d
Maciej Lasyk <maciej@lasyk.info>
2017-12-18 13:00:12 +0100

    pelican-sport: moved to another repo

---
+++ terraform/scripts/update_dlugodystansowy.pl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+GIT=/usr/bin/git
+
+export PROJDIR=/srv/dlugodystansowy.pl
+cd ${PROJDIR}
+$GIT fetch --all
+$GIT reset --hard origin/master
+docker rm pelican-dlugodystansowy.pl &> /dev/null
+docker run --name pelican-dlugodystansowy.pl -v ${PROJDIR}/pelican/content:/srv/content:Z \
+    -v ${PROJDIR}/pelican/output:/srv/output:Z \
+    -v ${PROJDIR}/pelican/pelicanconf.py:/srv/pelicanconf.py:Z \
+    -v ${PROJDIR}/pelican/publishconf.py:/srv/publishconf.py:Z \
+    -v ${PROJDIR}/pelican/themes:/srv/themes:Z \
+    -v ${PROJDIR}/pelican/plugins:/srv/plugins:Z gcr.io/maciej-lasyk-info/pelican-f25.1
