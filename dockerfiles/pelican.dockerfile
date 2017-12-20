1b863059a66af61ef804a1cd09d9ba4e73629bff
Maciej Lasyk <maciej@lasyk.info>
2017-12-20 23:08:10 +0100

    dlugodystansowy.pl conf: +PL locale

--- dockerfiles/pelican.dockerfile
+++ dockerfiles/pelican.dockerfile
@@ -2,7 +2,7 @@ FROM fedora:26
 MAINTAINER Maciej Lasyk <maciek@lasyk.info>
 
 RUN dnf -y install python-pip gcc make libxslt-devel redhat-rpm-config \
-    python-devel
+    python-devel langpacks-pl
 COPY requirements.txt /srv/requirements.txt
 RUN pip install --upgrade pip
 RUN pip install -r /srv/requirements.txt
--- terraform/scripts/update_dlugodystansowy.pl.sh
+++ terraform/scripts/update_dlugodystansowy.pl.sh
@@ -7,9 +7,9 @@ cd ${PROJDIR}
 $GIT fetch --all
 $GIT reset --hard origin/master
 docker rm pelican-dlugodystansowy.pl &> /dev/null
-docker run --name pelican-dlugodystansowy.pl -v ${PROJDIR}/pelican/content:/srv/content:Z \
+docker run --env LC_ALL=pl_PL --name pelican-dlugodystansowy.pl -v ${PROJDIR}/pelican/content:/srv/content:Z \
     -v ${PROJDIR}/pelican/output:/srv/output:Z \
     -v ${PROJDIR}/pelican/pelicanconf.py:/srv/pelicanconf.py:Z \
     -v ${PROJDIR}/pelican/publishconf.py:/srv/publishconf.py:Z \
     -v ${PROJDIR}/pelican/themes:/srv/themes:Z \
-    -v ${PROJDIR}/pelican/plugins:/srv/plugins:Z gcr.io/maciej-lasyk-info/pelican-f25.1
+    -v ${PROJDIR}/pelican/plugins:/srv/plugins:Z gcr.io/maciej-lasyk-info/pelican-f26-1
